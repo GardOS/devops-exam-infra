97ed43d53709a4b90c1d0b4b24a2ef52839137b0
GardOS <gardo1506@gmail.com>
2018-10-20 18:37:40 +0200

    Copy code from course

---
+++ terraform/provider_heroku.tf
@@ -0,0 +1,4 @@
+# Configure the Heroku provider
+provider "heroku" {
+  email = "gardo1506@gmail.com"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+# Modify this file to make it your own :-)
+
+variable "app_prefix" {
+  default = "gardos"
+}
+
+variable "pipeline_name" {
+  default = "exam-pipeline"
+}
