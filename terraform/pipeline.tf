dbd950ca270a0e4f95bdc715c36a3af601afadaf
GardOS <gardo1506@gmail.com>
2018-11-21 19:30:17 +0100

    Add changes from exam template

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -3,15 +3,17 @@ resource "heroku_pipeline" "test-app" {
   name = "${var.pipeline_name}"
 }
 
-#Ci
+#CI
 resource "heroku_app" "ci" {
   name   = "${var.app_prefix}-app-ci"
   region = "eu"
 }
+
 resource "heroku_addon" "db_ci" {
   app  = "${heroku_app.ci.name}"
   plan = "heroku-postgresql:hobby-dev"
 }
+
 resource "heroku_pipeline_coupling" "ci" {
   app      = "${heroku_app.ci.name}"
   pipeline = "${heroku_pipeline.test-app.id}"
@@ -23,10 +25,12 @@ resource "heroku_app" "staging" {
   name   = "${var.app_prefix}-app-staging"
   region = "eu"
 }
+
 resource "heroku_addon" "db_stage" {
   app  = "${heroku_app.staging.name}"
   plan = "heroku-postgresql:hobby-dev"
 }
+
 resource "heroku_pipeline_coupling" "staging" {
   app      = "${heroku_app.staging.name}"
   pipeline = "${heroku_pipeline.test-app.id}"
@@ -38,10 +42,12 @@ resource "heroku_app" "production" {
   name   = "${var.app_prefix}-app-production"
   region = "eu"
 }
+
 resource "heroku_addon" "db_prod" {
   app  = "${heroku_app.production.name}"
   plan = "heroku-postgresql:hobby-dev"
 }
+
 resource "heroku_pipeline_coupling" "production" {
   app      = "${heroku_app.production.name}"
   pipeline = "${heroku_pipeline.test-app.id}"
