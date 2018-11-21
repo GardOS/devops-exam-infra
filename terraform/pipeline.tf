9ccd2efc0fcb3daa6633582e2905e86f1dc7e592
GardOS <gardo1506@gmail.com>
2018-11-21 20:04:56 +0100

    Reorder pipeline.tf

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -3,27 +3,26 @@ resource "heroku_pipeline" "test-app" {
   name = "${var.pipeline_name}"
 }
 
-#CI
+# App
 resource "heroku_app" "ci" {
   name   = "${var.app_prefix}-app-ci"
   region = "eu"
 }
 
-resource "heroku_addon" "db_ci" {
-  app  = "${heroku_app.ci.name}"
-  plan = "heroku-postgresql:hobby-dev"
-}
-
-resource "heroku_pipeline_coupling" "ci" {
-  app      = "${heroku_app.ci.name}"
-  pipeline = "${heroku_pipeline.test-app.id}"
-  stage    = "development"
-}
-
-#Staging
 resource "heroku_app" "staging" {
   name   = "${var.app_prefix}-app-staging"
   region = "eu"
+}
+
+resource "heroku_app" "production" {
+  name   = "${var.app_prefix}-app-production"
+  region = "eu"
+}
+
+# Database
+resource "heroku_addon" "db_ci" {
+  app  = "${heroku_app.ci.name}"
+  plan = "heroku-postgresql:hobby-dev"
 }
 
 resource "heroku_addon" "db_stage" {
@@ -31,21 +30,22 @@ resource "heroku_addon" "db_stage" {
   plan = "heroku-postgresql:hobby-dev"
 }
 
+resource "heroku_addon" "db_prod" {
+  app  = "${heroku_app.production.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+# Coupling
+resource "heroku_pipeline_coupling" "ci" {
+  app      = "${heroku_app.ci.name}"
+  pipeline = "${heroku_pipeline.test-app.id}"
+  stage    = "development"
+}
+
 resource "heroku_pipeline_coupling" "staging" {
   app      = "${heroku_app.staging.name}"
   pipeline = "${heroku_pipeline.test-app.id}"
   stage    = "staging"
-}
-
-#Prod
-resource "heroku_app" "production" {
-  name   = "${var.app_prefix}-app-production"
-  region = "eu"
-}
-
-resource "heroku_addon" "db_prod" {
-  app  = "${heroku_app.production.name}"
-  plan = "heroku-postgresql:hobby-dev"
 }
 
 resource "heroku_pipeline_coupling" "production" {
