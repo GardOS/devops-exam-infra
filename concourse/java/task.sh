17650b8ae8da992e31b3eae2dd51a81b9959f057
GardOS <gardo1506@gmail.com>
2018-11-24 18:59:37 +0100

    Dev: Print file tree

--- concourse/java/task.sh
+++ concourse/java/task.sh
@@ -7,5 +7,8 @@ export NC='\033[0m'
 export CHECK="√"
 export M2_LOCAL_REPO=".m2"
 
+echo -e | pwd
+echo -e | ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
 mvn -f source/pom.xml install 
 echo -e "${GREEN}${CHECK} Maven install${NC}"
+echo -e | ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
