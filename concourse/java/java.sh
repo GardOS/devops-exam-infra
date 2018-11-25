e3fc3a6e97aaaf8ede52eec8c71ed0b4a25dbdec
GardOS <gardo1506@gmail.com>
2018-11-25 16:07:25 +0100

    Rename tasks and scripts

--- concourse/java/java.sh
+++ concourse/java/java.sh
@@ -7,9 +7,6 @@ export NC='\033[0m'
 export CHECK="√"
 export M2_LOCAL_REPO=".m2"
 
-echo -e | pwd
-find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
 mvn -f source/pom.xml install 
 echo -e "${GREEN}${CHECK} Maven install${NC}"
 cp -a source/. jar-file/
-find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
