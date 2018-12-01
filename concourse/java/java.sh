243e3fcf1fb27b9d6be2a080a4688679cd532274
GardOS <gardo1506@gmail.com>
2018-12-01 16:46:46 +0100

    Improve how app is deployed

--- concourse/java/java.sh
+++ concourse/java/java.sh
@@ -9,4 +9,6 @@ export M2_LOCAL_REPO=".m2"
 
 mvn -f source/pom.xml install 
 echo -e "${GREEN}${CHECK} Maven install${NC}"
-cp -a source/. jar-file/
+
+cp -a source/target/devops-exam-app-1.0-SNAPSHOT.jar ./app/devops-exam-app-1.0-SNAPSHOT.jar
+cp -a source/Dockerfile ./app/Dockerfile
